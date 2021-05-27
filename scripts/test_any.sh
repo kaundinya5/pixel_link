75134f1069f8cef3e88945fdb0e045a9094b6c67
kaundinya5 <kaundinya5@gmail.com>
2021-05-27 12:39:38 +0530

    Update test_any.sh

--- scripts/test_any.sh
+++ scripts/test_any.sh
@@ -6,8 +6,8 @@ export CUDA_VISIBLE_DEVICES=$1
 python test_pixel_link_on_any_image.py \
             --checkpoint_path=$2 \
             --dataset_dir=$3 \
-            --eval_image_width=1280\
-            --eval_image_height=768\
+            --eval_image_width=1180\
+            --eval_image_height=878\
             --pixel_conf_threshold=0.9\
             --link_conf_threshold=0.9\
             --gpu_memory_fraction=-1
