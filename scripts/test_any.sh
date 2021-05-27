c48fa1f24feb77773d98c807cd191f1de3cd8f06
kaundinya5 <kaundinya5@gmail.com>
2021-05-27 12:40:35 +0530

    Update test_any.sh

--- scripts/test_any.sh
+++ scripts/test_any.sh
@@ -7,7 +7,7 @@ python test_pixel_link_on_any_image.py \
             --checkpoint_path=$2 \
             --dataset_dir=$3 \
             --eval_image_width=1180\
-            --eval_image_height=878\
+            --eval_image_height=880\
             --pixel_conf_threshold=0.9\
             --link_conf_threshold=0.9\
             --gpu_memory_fraction=-1
